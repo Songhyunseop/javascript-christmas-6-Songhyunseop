be2332f81a707de44108a5921ca9cbd3fdb85a2b
songhyunseop <gustjq99455352@gmail.com>
2023-11-13 18:59:41 +0900

    Fix: 리팩토링으로 실패한 PromotionController 테스트케이스 수정
    - PromotionTest > OrderController 로 테스트파일 이름 변경

--- __tests__/OrderController.js
+++ __tests__/OrderController.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-lines-per-function */
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Promotion from '../src/Controller/Promotion';
+import OrderProcess from '../src/Controller/OrderController';
 
 const mockQuestions = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -22,9 +23,9 @@ const getLogSpy = () => {
 describe('잘못된 날짜 입력에 대한 예외처리', () => {
   test('범위 내의 날짜를 입력할 경우', async () => {
     mockQuestions(['99', '티본스테이크-1']);
-    const promotion = new Promotion();
+    const orderController = new OrderProcess();
 
-    await expect(promotion.readReservationInput()).rejects.toThrowError(
+    await expect(orderController.readReservationInput()).rejects.toThrowError(
       '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.'
     );
   });
@@ -35,9 +36,9 @@ describe('잘못된 날짜 입력에 대한 예외처리', () => {
   ])('숫자 이외의 값 입력할 경우', async ([reserveDay, orderMenus]) => {
     mockQuestions([reserveDay, orderMenus]);
 
-    const promotion = new Promotion();
+    const orderController = new OrderProcess();
 
-    expect(promotion.readReservationInput()).rejects.toThrowError(
+    expect(orderController.readReservationInput()).rejects.toThrowError(
       '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.'
     );
   });
@@ -47,9 +48,9 @@ describe('잘못된 메뉴 입력에 대한 예외처리', () => {
   test('메뉴에 없는 음식을 주문할 경우', async () => {
     mockQuestions(['9', '그냥가짜파스타-1,바비큐립-1']);
 
-    const promotion = new Promotion();
+    const orderController = new OrderProcess();
 
-    await expect(promotion.readReservationInput()).rejects.toThrowError(
+    await expect(orderController.readReservationInput()).rejects.toThrowError(
       '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.'
     );
   });
